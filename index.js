a13fd5d2cbcacd788786596566c07ed2dd413b21
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-10-27 19:57:40 -0300

    created api users

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+app.post("/api/users", async function(req, res){
+  const {username} = req.body
+  console.log(username)
+})
 
 
 
