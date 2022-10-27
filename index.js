73b247207f6461f5f1f450999f0f54089cf407a3
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-10-27 20:17:14 -0300

    POST api users and GET

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+
+const uri = process.env.MONGO_URI
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +20,23 @@ app.get('/', (req, res) => {
 
 app.post("/api/users", async function(req, res){
   const {username} = req.body
-  console.log(username)
+  client.connect(async err => {
+    if (err) throw err;
+    await client.db("users").collection("users.collection").insertOne({
+      'username': username
+    }).then(async x =>{
+     await client.db('users').collection('users.collection').findOne({'username': username}).then(async x =>{
+      await res.send(x)
+     })
+    })
+    
+    client.close()
+  });
+})
+app.get("/api/users", async function(req, res) {
+  await client.db('users').collection('users.collection').find().toArray().then(x =>{
+    res.send(x)
+  })
 })
 
 
